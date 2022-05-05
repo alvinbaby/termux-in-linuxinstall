fbcfe0c78f7b34a6f3b3ce8a94e766a967179e38
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-05-05 11:40:10 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,38 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-spinlong ()
-{
-    echo -e "\n"
-    bar=" Thanks for installing my tool................... "
-    barlength=${#bar}
-    i=0
-    while ((i < 100)); do
-        n=$((i*barlength / 100))
-        printf "\e[00;32m\r%-${barlength}s\e[00m" "${bar:0:n}"
-        ((i += RANDOM%5+2))
-        sleep 0.02
-    done
-}
-spinlong2 ()
-{
-    echo -e "\n"
-    bar=" enjoy the tool 😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂. "
-    barlength=${#bar}
-    i=0
-    while ((i < 100)); do
-        n=$((i*barlength / 100))
-        printf "\e[00;32m\r%-${barlength}s\e[00m" "${bar:0:n}"
-        ((i += RANDOM%5+2))
-        sleep 0.02
-    done
-}
-pkg update > /dev/null 2>&1 ; pkg upgrade -y
-pkg install tar -y > /dev/null 2>&1 ; pkg install curl -y > /dev/null 2>&1 ; pkg install proot -y > /dev/null 2>&1
-pkg install wget -y > /dev/null 2>&1
-pkg install curl -y > /dev/null 2>&1
-pkg install chroot proot -y > /dev/null 2>&1
-pkg install ruby -y > /dev/null 2>&1 ; gem install lolcat > /dev/null 2>&1
-echo "for Install Any Linux TyPe Name Of The Linux Likekali fedora ubuntu ubuntu20 arch alpine manjaro " | lolcat
-clear
+
 echo "Wait For 5 Sec " | lolcat
 sleep 3
 echo 
@@ -41,7 +8,12 @@ banner() {
 echo -e "\e[92m
 Linux installation ©\e[34m
 
-                                                                                                
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+-+-+
+    |t|e|r|m|u|x|-|i|n|-|l|i|n|u|x|i|n|s|t|a|l|l|
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+-+-+
+                +-+-+ +-+-+ +-+-+-+-+
+                |b|y| |M|R| |J|I|N|N|
+                +-+-+ +-+-+ +-+-+-+-+                                                                                        
                                                                                                 
 \e[m"
     echo -e "\e[1;31m"
